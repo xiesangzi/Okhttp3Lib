a1364c6e9a7ed62fa5a1070cfff484faefdb127a
xiesangzi <1358398521@qq.com>
2019-08-20 09:37:58 +0800

    优化子线程中执行网络请求

--- okhttp3/src/main/java/com/yhms/okhttp3/interceptor/Transformer.java
+++ okhttp3/src/main/java/com/yhms/okhttp3/interceptor/Transformer.java
@@ -44,8 +44,8 @@ public class Transformer {
                         loadingView.showLoadingView();
                     }
                 })
-                .subscribeOn(Schedulers.newThread())      // 子线程执行网络操作
-                .observeOn(AndroidSchedulers.mainThread()) // 转回主线程
+                .subscribeOn(Schedulers.io())     // 尽量避免使用Schedulers.newThread()每次都去创建一个线程，而是去使用Schedulers.io()可以去复用已有的线程，订阅处理在子线程中
+                .observeOn(AndroidSchedulers.mainThread()) // 转回主线程，订阅处理都在ui线程中
                 .doFinally(() -> {
                     if (loadingView != null) {
                         loadingView.hideLoadingView();
